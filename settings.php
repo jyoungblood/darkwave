bc21100ec9207b00c76b0afc5ab4b049a277c3a3
hxgf <jonathan.youngblood@gmail.com>
2022-12-31 12:32:36 -0600

    reset to stock settings

--- settings.php
+++ settings.php
@@ -1,22 +1,8 @@
 <?php
 
-// WARNING: THIS FILE IS GENERATED PROGRAMMATICALLY. ANY CHANGES YOU MAKE MAY BE OVERWRITTEN.
-
-$GLOBALS['site_title'] = 'Test DW Install';
-$GLOBALS['site_code'] = 'TSTDW';
-$GLOBALS['site_url'] = 'dev.darkwave.ltd';
-$GLOBALS['settings']['mode'] = 'development';
+$GLOBALS['site_url'] = $_SERVER['HTTP_HOST'];
 
 $GLOBALS['locals'] = [
   'year' => date('Y'),
-  'site_title' => $GLOBALS['site_title'],
-  'site_code' => $GLOBALS['site_code'],
   'site_url' => $GLOBALS['site_url'],
-];
-
-$GLOBALS['settings']['database'] = [
-  'host' => 'localhost',
-  'name' => 'hxgf_dwdemo',
-  'user' => 'hxgf_dwdemo',
-  'password' => '0000demo'
 ];
