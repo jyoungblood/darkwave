79d62a8da14598e4544ceb5275e40d048378c796
jonathan youngblood <jonathan.youngblood@gmail.com>
2023-04-22 13:29:40 -0500

    finish header tweaks, navbar demo

--- controllers/index.php
+++ controllers/index.php
@@ -22,3 +22,58 @@ $app->get('/', function ($req, $res, $args) {
 
 
 
+
+
+
+
+$app->get('/demo/{page}[/]', function ($req, $res, $args) {
+
+  $subnav = false;
+
+  if ($args['page'] == 'first'){
+    $subnav = [
+      [
+        'href' => '?subnav=1',
+        'title' => 'Subnav 1'
+      ], 
+      [
+        'href' => '?subnav=2',
+        'title' => 'Subnav 2'
+      ],
+      [
+        'href' => '?subnav=3',
+        'title' => 'Subnav 3'
+      ]
+    ];
+  }
+
+  if ($args['page'] == 'second'){
+    $subnav = [
+      [
+        'href' => '?subnav=different-subnav',
+        'title' => 'Different Subnav'
+      ], 
+      [
+        'href' => '?subnav=ok-cool',
+        'title' => 'OK Cool'
+      ],
+      [
+        'href' => '?subnav=new-thing',
+        'title' => '+ New Thing'
+      ]
+    ];
+  }
+
+  return render::hbs($req, $res, [
+    'layout' => '_layouts/base',
+    'template' => 'index',
+    'title' => 'DEMO ' . $args['page'] . ' - ' . $GLOBALS['site_title'],
+    'data' => [
+      'current_first' => $args['page'] == 'first' ? true : false,
+      'current_second' => $args['page'] == 'second' ? true : false,
+      'current_third' => $args['page'] == 'third' || $args['page'] == 'fourth' || $args['page'] == 'fifth' || $args['page'] == 'sixth' ? true : false,
+      'subnav' => $subnav,
+    ]
+  ]);
+  
+});
--- settings.php
+++ settings.php
@@ -3,11 +3,5 @@
 $GLOBALS['site_url'] = $_SERVER['HTTP_HOST'];
 
 $GLOBALS['locals'] = [
-  'year' => date('Y'),
-  'site_title' => $GLOBALS['site_title'],
-  'site_code' => $GLOBALS['site_code'],
-  'site_url' => $GLOBALS['site_url'],
-  'auth' => @$GLOBALS['auth'],
-  'user_id' => @$GLOBALS['user_id'],
-  'is_admin' => @$GLOBALS['is_admin'],
+  'year' => date('Y')
 ];
