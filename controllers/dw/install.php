3700969a53023362d5087b3804accb1a1873df72
jonathan youngblood <jonathan.youngblood@gmail.com>
2023-05-01 17:54:07 -0500

    installation updates

--- controllers/dw/install.php
+++ controllers/dw/install.php
@@ -6,140 +6,156 @@ use VPHP\db;
 
 
 $app->get('/install[/]', function ($req, $res, $args) {
+
+  $digits    = array_flip(range('0', '9'));
+  $lowercase = array_flip(range('a', 'z'));
+  $uppercase = array_flip(range('A', 'Z')); 
+  $special   = array_flip(str_split('*&!@%^#$'));
+
   return render::hbs($req, $res, [
     'layout' => '_layouts/base-guest',
     'template' => 'dw/install',
     'title' => 'Install Darkwave',
+    'data' => [
+      'jwt_secret' => str_shuffle(array_rand($digits) . array_rand($lowercase) . array_rand($uppercase) . array_rand($special) . implode(array_rand(array_merge($digits, $lowercase, $uppercase, $special), rand(12, 20)))),
+      'site_url_default' => $_SERVER['HTTP_HOST']
+    ]
   ]);
 });
 
 
 
-$app->post('/execute-install[/]', function ($req, $res, $args) {
+$app->post('/install/execute[/]', function ($req, $res, $args) {
 
-	// 1. make the settings file
+    parse_str($_POST['form'],$form);
 
-	$settings_file = '<?php
-
-$GLOBALS[\'site_title\'] = \''.$_POST['site_title'].'\';
-$GLOBALS[\'site_code\'] = \''.$_POST['site_code'].'\';
-$GLOBALS[\'site_url\'] = \''.$_POST['site_url'].'\';
-$GLOBALS[\'settings\'][\'mode\'] = \'development\';
-
-$GLOBALS[\'locals\'] = [
-  \'year\' => date(\'Y\'),
-  \'site_title\' => $GLOBALS[\'site_title\'],
-  \'site_code\' => $GLOBALS[\'site_code\'],
-  \'site_url\' => $GLOBALS[\'site_url\'],
-  \'auth\' => @$GLOBALS[\'auth\'],
-  \'user_id\' => @$GLOBALS[\'user_id\'],
-  \'is_admin\' => @$GLOBALS[\'is_admin\'],
-];
-';
-
-	if ($_POST['db_host'] && $_POST['db_name']){
-		$settings_file .= '
-$GLOBALS[\'settings\'][\'database\'] = [
-  \'host\' => \''.$_POST['db_host'].'\',
-  \'name\' => \''.$_POST['db_name'].'\',
-  \'user\' => \''.$_POST['db_user'].'\',
-  \'password\' => \''.$_POST['db_password'].'\'
-];';
-	}
-
-	file_put_contents("./settings.php", $settings_file);
-
-
-
-	// 2. make the db tables (only if there's a database to connect to)
-
-	$error = false;
-	$db_install = false;
-
-	if ($_POST['db_host'] && $_POST['db_name']){
-
-		$db_install = true;
-
-		try {
-
-      $GLOBALS['database'] = db::init([
-        'host' => $_POST['db_host'],
-        'name' => $_POST['db_name'],
-        'user' => $_POST['db_user'],
-        'password' => $_POST['db_password']
-      ]);
-
-			$sql = "CREATE TABLE users (
-			id INT(255) NOT NULL AUTO_INCREMENT,
-			_id VARCHAR(255) NULL DEFAULT NULL,
-			password VARCHAR(255) NULL DEFAULT NULL,
-			email VARCHAR(255) NULL DEFAULT NULL,
-			validate_hash VARCHAR(255) NULL DEFAULT NULL,
-			password_hash VARCHAR(255) NULL DEFAULT NULL,
-			url_slug VARCHAR(255) NULL DEFAULT NULL COMMENT 'profile url handle (url-safe version of screenname)',
-			screenname VARCHAR(255) NULL DEFAULT NULL COMMENT 'display name',
-			first_name VARCHAR(255) NULL DEFAULT NULL,
-			last_name VARCHAR(255) NULL DEFAULT NULL,
-			ua_header VARCHAR(255) NULL DEFAULT NULL,
-			ip_address VARCHAR(255) NULL DEFAULT NULL,
-			group_id TINYINT NOT NULL DEFAULT '3' COMMENT '1 Admin, 2 Moderator, 3 General User, 4 Blocked User',
-			avatar_filename VARCHAR(255) DEFAULT NULL,
-			avatar_small VARCHAR(255) NOT NULL DEFAULT '/images/avatar-default.png',
-			avatar_medium VARCHAR(255) NOT NULL DEFAULT '/images/avatar-default.png',
-			avatar_large VARCHAR(255) NOT NULL DEFAULT '/images/avatar-default.png',
-			avatar_original VARCHAR(255) NOT NULL DEFAULT '/images/avatar-default.png',
-			date_last_login DATETIME NULL DEFAULT NULL,
-			date_created DATETIME NULL DEFAULT NULL,
-			date_updated DATETIME NULL DEFAULT NULL,
-			PRIMARY KEY (id)
-			) ENGINE=InnoDB CHARACTER SET utf8mb4;";
-
-			$GLOBALS['database']->exec($sql);
-      
-		}
-		catch(PDOException $e){
-			$error = $e->getMessage();
-		}
-
-
-
-
-		// 3. insert the admin user
-		if ($_POST['user_email']){
-			if (!$error){
-				db::insert("users", [
-					'_id' => uniqid(uniqid()),
-					'email' => strtolower($_POST['user_email']),
-					'password' => password_hash($_POST['user_password'], PASSWORD_BCRYPT),
-					'group_id' => '1',
-					'date_created' => date('Y-m-d H:i:s'),
-					'url_slug' => \VPHP\x::url_slug($_POST['user_screenname']),
-					'screenname' => $_POST['user_screenname'],
-				]);
-			}
-		}
-
-
-	}
-
-
-	// 4. show success/failure
-
-	if ($error){
-
-    return render::hbs($req, $res, [
-      'layout' => '_layouts/base-guest',
-      'template' => 'dw/install',
-      'title' => $_ENV['SITE_TITLE'] ? $_ENV['SITE_TITLE'] : 'Install Darkwave',
-      'data' => [
-        'error' => $error
-      ]
+    return render::json($req, $res, [
+      'form' => $form
     ]);
 
-	}else{
-		unlink("controllers/install.php");
-    return render::redirect($req, $res, '/');
-	}
+// 	// 1. make the settings file
+
+// 	$settings_file = '<?php
+
+// $GLOBALS[\'site_title\'] = \''.$_POST['site_title'].'\';
+// $GLOBALS[\'site_code\'] = \''.$_POST['site_code'].'\';
+// $GLOBALS[\'site_url\'] = \''.$_POST['site_url'].'\';
+// $GLOBALS[\'settings\'][\'mode\'] = \'development\';
+
+// $GLOBALS[\'locals\'] = [
+//   \'year\' => date(\'Y\'),
+//   \'site_title\' => $GLOBALS[\'site_title\'],
+//   \'site_code\' => $GLOBALS[\'site_code\'],
+//   \'site_url\' => $GLOBALS[\'site_url\'],
+//   \'auth\' => @$GLOBALS[\'auth\'],
+//   \'user_id\' => @$GLOBALS[\'user_id\'],
+//   \'is_admin\' => @$GLOBALS[\'is_admin\'],
+// ];
+// ';
+
+// 	if ($_POST['db_host'] && $_POST['db_name']){
+// 		$settings_file .= '
+// $GLOBALS[\'settings\'][\'database\'] = [
+//   \'host\' => \''.$_POST['db_host'].'\',
+//   \'name\' => \''.$_POST['db_name'].'\',
+//   \'user\' => \''.$_POST['db_user'].'\',
+//   \'password\' => \''.$_POST['db_password'].'\'
+// ];';
+// 	}
+
+// 	file_put_contents("./settings.php", $settings_file);
+
+
+
+// 	// 2. make the db tables (only if there's a database to connect to)
+
+// 	$error = false;
+// 	$db_install = false;
+
+// 	if ($_POST['db_host'] && $_POST['db_name']){
+
+// 		$db_install = true;
+
+// 		try {
+
+//       $GLOBALS['database'] = db::init([
+//         'host' => $_POST['db_host'],
+//         'name' => $_POST['db_name'],
+//         'user' => $_POST['db_user'],
+//         'password' => $_POST['db_password']
+//       ]);
+
+// 			$sql = "CREATE TABLE users (
+// 			id INT(255) NOT NULL AUTO_INCREMENT,
+// 			_id VARCHAR(255) NULL DEFAULT NULL,
+// 			password VARCHAR(255) NULL DEFAULT NULL,
+// 			email VARCHAR(255) NULL DEFAULT NULL,
+// 			validate_hash VARCHAR(255) NULL DEFAULT NULL,
+// 			password_hash VARCHAR(255) NULL DEFAULT NULL,
+// 			url_slug VARCHAR(255) NULL DEFAULT NULL COMMENT 'profile url handle (url-safe version of screenname)',
+// 			screenname VARCHAR(255) NULL DEFAULT NULL COMMENT 'display name',
+// 			first_name VARCHAR(255) NULL DEFAULT NULL,
+// 			last_name VARCHAR(255) NULL DEFAULT NULL,
+// 			ua_header VARCHAR(255) NULL DEFAULT NULL,
+// 			ip_address VARCHAR(255) NULL DEFAULT NULL,
+// 			group_id TINYINT NOT NULL DEFAULT '3' COMMENT '1 Admin, 2 Moderator, 3 General User, 4 Blocked User',
+// 			avatar_filename VARCHAR(255) DEFAULT NULL,
+// 			avatar_small VARCHAR(255) NOT NULL DEFAULT '/images/avatar-default.png',
+// 			avatar_medium VARCHAR(255) NOT NULL DEFAULT '/images/avatar-default.png',
+// 			avatar_large VARCHAR(255) NOT NULL DEFAULT '/images/avatar-default.png',
+// 			avatar_original VARCHAR(255) NOT NULL DEFAULT '/images/avatar-default.png',
+// 			date_last_login DATETIME NULL DEFAULT NULL,
+// 			date_created DATETIME NULL DEFAULT NULL,
+// 			date_updated DATETIME NULL DEFAULT NULL,
+// 			PRIMARY KEY (id)
+// 			) ENGINE=InnoDB CHARACTER SET utf8mb4;";
+
+// 			$GLOBALS['database']->exec($sql);
+      
+// 		}
+// 		catch(PDOException $e){
+// 			$error = $e->getMessage();
+// 		}
+
+
+
+
+// 		// 3. insert the admin user
+// 		if ($_POST['user_email']){
+// 			if (!$error){
+// 				db::insert("users", [
+// 					'_id' => uniqid(uniqid()),
+// 					'email' => strtolower($_POST['user_email']),
+// 					'password' => password_hash($_POST['user_password'], PASSWORD_BCRYPT),
+// 					'group_id' => '1',
+// 					'date_created' => date('Y-m-d H:i:s'),
+// 					'url_slug' => \VPHP\x::url_slug($_POST['user_screenname']),
+// 					'screenname' => $_POST['user_screenname'],
+// 				]);
+// 			}
+// 		}
+
+
+// 	}
+
+
+// 	// 4. show success/failure
+
+// 	if ($error){
+
+//     return render::hbs($req, $res, [
+//       'layout' => '_layouts/base-guest',
+//       'template' => 'dw/install',
+//       'title' => $_ENV['SITE_TITLE'] ? $_ENV['SITE_TITLE'] : 'Install Darkwave',
+//       'data' => [
+//         'error' => $error
+//       ]
+//     ]);
+
+// 	}else{
+// 		unlink("controllers/install.php");
+//     return render::redirect($req, $res, '/');
+// 	}
 
 });
 
