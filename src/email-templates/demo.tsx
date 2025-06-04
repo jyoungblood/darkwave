4ead272ae96119695d71ac185011ab48d3b11a62
jonathan youngblood <jonathan.youngblood@gmail.com>
2025-06-04 10:46:25 -0700

    cleanup

--- src/email-templates/demo.tsx
+++ src/email-templates/demo.tsx
@@ -1,11 +1,9 @@
-
 export function render(data: any) {
   return `
     <center>
-      <h1>TEST EMAIL FROM TEST.TSX</h1>
-      <p>From: ${data.name} ${data.surname}</p>
+      <h1>TEST EMAIL FROM DEMO.TSX</h1>
+      <p>From: ${data.name}</p>
       <p>Email: ${data.email}</p>
-      <p>Tel: ${data.tel}</p>
       ${data.message ? `<p>Message: ${data.message.replace(/\n/g, '<br />')}</p>` : ''}
     </center>
   `;
